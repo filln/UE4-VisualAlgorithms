4056d88189ed20ebe789e88d715f0262454bcbc4
filln <ulv0710@gmail.com>
2020-05-12 19:06:43 +0300

    Add project files.

---
+++ Source/VisualAlgorithms/VisualAlgorithms.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "VisualAlgorithms.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VisualAlgorithms, "VisualAlgorithms" );
---
+++ Source/VisualAlgorithms/VisualAlgorithmsGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VisualAlgorithmsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VISUALALGORITHMS_API AVisualAlgorithmsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
